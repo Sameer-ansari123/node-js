7ac464ec8951c66f6b4d1cf924c2e02d0db8d156
Sameer <ansarisameerrehan@gmail.com>
2024-12-23 12:16:38 +0000

    changes

--- index.mjs
+++ index.mjs
@@ -4,17 +4,5 @@ console.log(chalk.bgYellow('Hello world!'));
 console.log(chalk.yellow('Hello world!'));
 console.log(chalk.red('Hello world!'));
 
-const { spawn } = require('node:child_process');
-const ls = spawn('ls', ['-lh', '/usr']);
 
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
 
-ls.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
